5a5cf603bfc9a0e883493fe4a56aaee563cd4162
Lena Kwan <lenakwan@gmail.com>
2021-01-13 16:16:07 -0800

    fix routing

--- api/v1/api.js
+++ api/v1/api.js
@@ -12,9 +12,14 @@ api.use(bodyParser.urlencoded({ extended: false }))
 api.use(bodyParser.json()) 
 api.use(cors());
 
+//fix routing to stop displaying CANNOT GET /
 api.get('/', (req, res) => res
   .send({
     message: 'Hello, this is the API that I made for the shopify Summer 2021 Intern Challenge! If you need help with using this API please contact me at lenakwan@gmail.com'
   }));
 
+  api.get('*', (req, res) => res
+  .send({
+    message: 'Hello, this is the API that I made for the shopify Summer 2021 Intern Challenge! If you need help with using this API please contact me at lenakwan@gmail.com'
+  }));
 module.exports= api;
