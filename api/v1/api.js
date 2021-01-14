f35032ffb0e9cdcbe3d8f077a180040c7f2ca20f
Lena Kwan <lenakwan@gmail.com>
2021-01-13 16:08:56 -0800

    fix for api call

--- api/v1/api.js
+++ api/v1/api.js
@@ -13,6 +13,9 @@ api.use(bodyParser.urlencoded({ extended: false }))
 api.use(bodyParser.json()) 
 api.use(cors());
 
-api.get('/', landingpage.landingResponse);
+api.get('/', (req, res) => res
+  .send({
+    message: 'Hello, this is the API that I made for the shopify Summer 2021 Intern Challenge! If you need help with using this API please contact me at lenakwan@gmail.com'
+  }));
 
 module.exports= api;
