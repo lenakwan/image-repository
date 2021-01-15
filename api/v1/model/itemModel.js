a2802a8492b63c8ff0a1a7cb8877e8cd5ce22b99
Lena Kwan <lenakwan@gmail.com>
2021-01-14 19:16:37 -0800

    fix async error

--- api/v1/model/itemModel.js
+++ api/v1/model/itemModel.js
@@ -1,6 +1,6 @@
 const db = require('../database');
 
-addItem(item_name, image_text, item_category, quantity, private, price, discount, user_id) = async ()=> {
+addItem= async (item_name, image_text, item_category, quantity, private, price, discount, user_id)=> {
     return await db.pool.query(`INSERT INTO items (item_name, item_category, list_date, quantity, private
         , price, discount, img_id, fk_user_id) VALUES('${item_name}', '${image_text}', '${item_category}', current_timestamp, '${quantity}'
         , '${private}', '${price}', '${discount}', nextval('items_img_id_seq'), '${user_id}')`);
